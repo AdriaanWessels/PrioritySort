05a1880457186774c226480f02ac6d342295fe3b
AdriaanWessels <adriaan@khenti.com>
2018-01-11 19:50:30 +0200

    Added multiple buttons

--- App.jsx
+++ App.jsx
@@ -5,21 +5,46 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      data: "Initial data..."
+      data: "Initial data...",
+      buttonA: "testA",
+      buttonB: "testB",
+      i: 1,
+      j: 1
     };
     this.updateState = this.updateState.bind(this);
+    this.sortData = this.sortData.bind(this);
   }
+
   updateState(e) {
     this.setState({ data: e.target.value });
   }
+
+  sortData(e) {
+    let dataArr = this.state.data.split("\n");
+
+    this.setState({
+      buttonA: "clickA",
+      buttonB: "clickB",
+      data: dataArr
+    });
+  }
+
   render() {
     return (
       <div>
-        <input
-          type="text"
+        <h1>Enter your list below</h1>
+        <textarea
+          rows="10"
+          cols="50"
           value={this.state.data}
           onChange={this.updateState}
         />
+        <hr />
+        <input type="button" value="sort" onClick={this.sortData} />
+        <hr />
+        <input type="button" value={this.state.buttonA} />
+        <br />
+        <input type="button" value={this.state.buttonB} />
         <h4>{this.state.data}</h4>
       </div>
     );
