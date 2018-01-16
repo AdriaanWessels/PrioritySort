3df94cb22e078716fc15ac179d2fa2bdcde24f39
AdriaanWessels <adriaan@khenti.com>
2018-01-16 22:22:51 +0200

    add minimal insertion sort capability

--- App.jsx
+++ App.jsx
@@ -1,3 +1,4 @@
+"use strict";
 import React from "react";
 
 class App extends React.Component {
@@ -5,9 +6,10 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      data: "Initial data...",
-      buttonA: "testA",
-      buttonB: "testB",
+      data: "c\na\nb",
+      dataArr: [],
+      buttonA: "",
+      buttonB: "",
       i: 1,
       j: 1
     };
@@ -21,14 +23,51 @@ class App extends React.Component {
 
   sortData(e) {
     let dataArr = this.state.data.split("\n");
-    dataArr.push("test!");
-    let newData = dataArr.join("\n");
-    console.log("test!");
-    this.setState({
-      buttonA: "clickA",
-      buttonB: "clickB",
-      data: newData
-    });
+    console.log(dataArr);
+    console.log("value = " + e.target.value);
+    console.log("start: i:" + this.state.i + "   j:" + this.state.j);
+    // perform the next step of the insertion sort
+    if (e.target.value !== "sort") {
+      if (this.state.j > 0 && e.target.value === this.state.buttonA) {
+        // swap dataArr[j] and dataArr[j-1]
+        console.log("swap " + this.state.j + " and " + (this.state.j - 1));
+        console.log("swap:  i:" + this.state.i + "   j:" + this.state.j);
+
+        let swapTmp = dataArr[this.state.j];
+        dataArr[this.state.j] = dataArr[this.state.j - 1];
+        dataArr[this.state.j - 1] = swapTmp;
+
+        console.log(dataArr);
+
+        // update the data in the textarea
+        let newData = dataArr.join("\n");
+        this.setState({
+          data: newData
+        });
+
+        this.state.j--;
+      } else {
+        this.state.i++;
+        this.state.j = this.state.i;
+        console.log("adv:   i:" + this.state.i + "   j:" + this.state.j);
+      }
+    }
+
+    console.log("end:   i:" + this.state.i + "   j:" + this.state.j);
+
+    console.log(this.state.i + "<" + dataArr.length);
+
+    if (this.state.i < dataArr.length) {
+      this.setState({
+        buttonA: dataArr[this.state.j - 1],
+        buttonB: dataArr[this.state.j]
+      });
+    } else {
+      this.setState({
+        buttonA: "",
+        buttonB: ""
+      });
+    }
   }
 
   render() {
@@ -44,9 +83,17 @@ class App extends React.Component {
         <hr />
         <input type="button" value="sort" onClick={this.sortData} />
         <hr />
-        <input type="button" value={this.state.buttonA} />
+        <input
+          type="button"
+          value={this.state.buttonA}
+          onClick={this.sortData}
+        />
         <br />
-        <input type="button" value={this.state.buttonB} />
+        <input
+          type="button"
+          value={this.state.buttonB}
+          onClick={this.sortData}
+        />
         <h4>{this.state.data}</h4>
       </div>
     );
