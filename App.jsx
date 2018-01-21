84eb21dfeb27b6343bf60d8fe52085f25c6fc2c6
AdriaanWessels <adriaan@khenti.com>
2018-01-21 23:52:12 +0200

    Add binary search to the sort

--- App.jsx
+++ App.jsx
@@ -6,12 +6,12 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      data: "c\na\nb",
-      dataArr: [],
+      data: "a\nb\nh\nc\ng\nd\ne\nf",
       buttonA: "",
       buttonB: "",
       i: 1,
-      j: 1
+      hi: 1,
+      lo: 0
     };
     this.updateState = this.updateState.bind(this);
     this.sortData = this.sortData.bind(this);
@@ -23,50 +23,49 @@ class App extends React.Component {
 
   sortData(e) {
     let dataArr = this.state.data.split("\n");
-    console.log(dataArr);
-    console.log("value = " + e.target.value);
-    console.log("start: i:" + this.state.i + "   j:" + this.state.j);
-    // perform the next step of the insertion sort
-    if (e.target.value !== "sort") {
-      if (this.state.j > 0 && e.target.value === this.state.buttonA) {
-        // swap dataArr[j] and dataArr[j-1]
-        console.log("swap " + this.state.j + " and " + (this.state.j - 1));
-        console.log("swap:  i:" + this.state.i + "   j:" + this.state.j);
-
-        let swapTmp = dataArr[this.state.j];
-        dataArr[this.state.j] = dataArr[this.state.j - 1];
-        dataArr[this.state.j - 1] = swapTmp;
-
-        console.log(dataArr);
-
-        // update the data in the textarea
-        let newData = dataArr.join("\n");
-        this.setState({
-          data: newData
-        });
-
-        this.state.j--;
-      } else {
-        this.state.i++;
-        this.state.j = this.state.i;
-        console.log("adv:   i:" + this.state.i + "   j:" + this.state.j);
-      }
-    }
-
-    console.log("end:   i:" + this.state.i + "   j:" + this.state.j);
-
-    console.log(this.state.i + "<" + dataArr.length);
+    let mid = Math.floor((this.state.hi + this.state.lo) / 2);
 
     if (this.state.i < dataArr.length) {
-      this.setState({
-        buttonA: dataArr[this.state.j - 1],
-        buttonB: dataArr[this.state.j]
-      });
-    } else {
-      this.setState({
-        buttonA: "",
-        buttonB: ""
-      });
+      // perform the next step of the binary search
+      switch (e.target.value) {
+        case this.state.buttonA:
+          this.state.lo = mid + 1;
+          break;
+
+        case this.state.buttonB:
+          this.state.hi = mid - 1;
+          break;
+      }
+
+      if (this.state.lo > this.state.hi) {
+        // if the binary search is done insert A[i] in the correct place
+        let tmpItem = dataArr[this.state.i];
+        dataArr.splice(this.state.i, 1);
+        dataArr.splice(this.state.lo, 0, tmpItem);
+        this.setState({
+          data: dataArr.join("\n")
+        });
+        // move to the next element
+        this.state.i++;
+        this.state.hi = this.state.i;
+        this.state.lo = 0;
+      }
+
+      if (this.state.i < dataArr.length) {
+        // update the button text
+        let newMid = Math.floor((this.state.hi + this.state.lo) / 2);
+        console.log("newMid:" + newMid);
+        this.setState({
+          buttonA: dataArr[newMid],
+          buttonB: dataArr[this.state.i]
+        });
+      } else {
+        // data is sorted
+        this.setState({
+          buttonA: "",
+          buttonB: ""
+        });
+      }
     }
   }
 
